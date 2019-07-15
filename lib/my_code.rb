fdd647300608d1c21dd079ac087ec15d1c93f2ab
Hanna Lee <mani21na@gmail.com>
2019-07-15 16:52:06 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,36 +1,26 @@
 def map_to_negativize(source_array)
-  new = []
-  i = 0
-  while i < source_array.length do
-    new.push( source_array[i] * -1 ) # <== Unique work
-    i += 1
-  end
-  return new
+  source_array.map{|n| n * -1}
 end
+
 def map_to_no_change(source_array)
-  new = []
-  i = 0
-  while i < source_array.length do
-    new.push( source_array[i] ) # <== Unique work
-    i += 1
-  end
-  return new
+  source_array.map{|n| n}
 end
+
 def map_to_double(source_array)
-  new = []
-  i = 0
-  while i < source_array.length do
-    new.push( source_array[i] * 2 ) # <== Unique work
-    i += 1
-  end
-  return new
+  source_array.map{|n| n * 2}
 end
+
 def map_to_square(source_array)
-  new = []
+  source_array.map{|n| n ** 2}
+end
+
+def reduce_to_total(source_array, starting_point = 0)
+  source_array.reduce(starting_point){ |sum, num| sum + num }
+end
+
+def reduce_to_all_true(source_array)
   i = 0
   while i < source_array.length do
-    new.push( source_array[i] * source_array[i] ) # <== Unique work
+    return false if !source_array[i]
     i += 1
   end
-  return new
-end
